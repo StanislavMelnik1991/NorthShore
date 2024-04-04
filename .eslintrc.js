aaa780e9ff56f228262a2bc0fda536c92380560f
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-04-04 12:32:54 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "@typescript-eslint/eslint-plugin", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@conarti/feature-sliced/recommended",
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    es2021: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  rules: {},
+};
