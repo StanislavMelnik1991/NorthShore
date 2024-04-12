f5e8d40f10ecdb2ba4d56b506a9854db209b6368
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-04-12 11:40:42 +0300

    remove detection language

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,11 +1,9 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "ru",
@@ -15,22 +13,6 @@ i18n
     },
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
-    },
-    detection: {
-      order: [
-        "querystring",
-        "cookie",
-        "localStorage",
-        "sessionStorage",
-        "navigator",
-        "htmlTag",
-        "path",
-        "subdomain",
-      ],
-      lookupQuerystring: "lng",
-      caches: ["cookie"],
-      checkWhitelist: true,
-      whitelist: ["en", "ru"],
     },
   });
 
