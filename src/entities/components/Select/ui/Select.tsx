9f4dbeb773d3e1848978692ab8b11f3e73488a64
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-04-24 12:35:22 +0300

    update select styles

--- src/entities/components/Select/ui/Select.tsx
+++ src/entities/components/Select/ui/Select.tsx
@@ -46,6 +46,9 @@ export const StyledSelect = ({
             }
             return styles.option;
           },
+          input: () => {
+            return styles.input;
+          },
         }}
         styles={{
           control: (baseStyles) => ({
